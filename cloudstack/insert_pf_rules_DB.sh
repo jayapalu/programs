44b4e3dd296014d6474e1fd4ad5ebe306e49725b
Jayapal <jayapal.uradi@accelerite.com>
2017-10-26 18:26:32 +0530

    added script to insert pf rules into cloudstack DB

---
+++ cloudstack/insert_pf_rules_DB.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# script for inserting portforwarding rules into the cloudstack DB
+
+networkid=305
+ipid=9
+instance_id=325
+#vm ip address
+pfvmip=10.1.1.126
+
+#set -x
+for i in {20..30}
+do
+
+	a=$(uuidgen)
+	uuid=$(echo "$a" | tr '[:upper:]' '[:lower:]')
+
+	mysql -uroot --database cloud -bse "INSERT INTO firewall_rules
+	(id, uuid, ip_address_id, start_port, end_port, state, protocol, purpose, account_id, domain_id, network_id, xid, created, icmp_code, icmp_type, related, type, vpc_id, traffic_type, display)
+	VALUES ($i,UUID(), $ipid, $i, $i, 'Active', 'tcp', 'PortForwarding', 2, 1, $networkid, '$uuid', now(), NULL, NULL, NULL, 'User', NULL, NULL, 1)"
+	echo "Ports $i - $i Inserting into * firewall_rules rule *  success=$?"
+	mysql -uroot --database cloud -bse "INSERT INTO port_forwarding_rules (id, instance_id,dest_ip_address,dest_port_start, dest_port_end) VALUES($i,$instance_id,'$pfvmip',$i,$i);" 
+
+	echo "Ports $i - $i Inserting into * port_forwarding_rules * rule success=$?"
+
+done
