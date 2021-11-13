53b931d4febab2894101ecd4628d3fa425b1f2c8
Jayapal Uradi <jayapal_uradi@persistent.com>
2021-11-13 11:44:15 +0530

    Added binary searh programs

--- JavaPractice/leetcode/BinarySearchRotated.java
+++ JavaPractice/leetcode/BinarySearchRotated.java
@@ -1,10 +1,10 @@
-public class BinarySearch {
+public class BinarySearchRotated {
 
     public static void main(String[] args){
 
-        int[] num = {1,2,3,4,5,6,7,8};
+        int[] num = {4,5,6,7,0,1,2,3};
 
-        binarySearch(num,0,7,5);
+        binarySearch(num,0,7,9);
     }
 
     public static int binarySearch(int[] a, int low, int high, int key) {
@@ -18,13 +18,16 @@ public class BinarySearch {
                 return key;
             }
 
-            if (key < a[mid]) {
+            if (key < a[mid] && a[low] <= key) {
+                System.out.println(" key < mid");
                 high = mid-1;
             }else {
+                System.out.println(" key > mid");
                 low = mid+1;
             }
         }
 
+        System.out.println("key not found");
         return -1;
 
     } //method
